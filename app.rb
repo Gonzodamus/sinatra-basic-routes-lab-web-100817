9ef6a30e4358e92e7419d0d6c2d0fe35c6a94600
Gonzodamus <Dick@TheDickWard.com>
2017-10-29 11:30:07 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+  "My name is Dick!"
+end
+
+get '/hometown' do
+  "My hometown is Novi"
+end
+
+get '/favorite-song' do
+  "My favorite song is There's a Great Big Beautiful Tomorrow"
+end
+
 end
